a4ca4fd4c8072e0953b3477651cd5666ecd6393f
RISHABH KUMAR MISHRA <51675320+mrishabh781@users.noreply.github.com>
2020-07-30 21:17:08 +0530

    Add files via upload

---
+++ qr_code_gen.py
@@ -0,0 +1,26 @@
+from kivy.app import App
+from kivy.lang import Builder
+from kivy.uix.widget import Widget
+from kivy_garden.qrcode import QRCodeWidget
+
+class MyWidget(Widget):
+    parent=Widget()
+    parent.add_widget(QRCodeWidget(data="Kivy Rocks"))
+root=Builder.load_string('''
+
+
+#:import QRCodeWidget kivy_garden.qrcode
+
+BoxLayout:
+    orientation: 'vertical'
+    QRCodeWidget:
+        id: qr
+        data: '35338509553035638'
+        ''')
+class GeneratorApp(App):
+    def build(self):
+        return root
+
+
+if __name__ == "__main__":
+    GeneratorApp().run()
